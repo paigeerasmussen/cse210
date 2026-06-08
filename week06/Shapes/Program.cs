eae40a3bb16bbceef64eeaeacdce3cdfa2483a65
paigeerasmussen <paigeerasmussen@gmail.com>
2026-06-08 15:35:23 -0600

    Completed Shapes Learning Activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,26 @@
 using System;
 
 class Program
-{
-    static void Main(string[] args)
+{ static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("blue", 5);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("green", 5, 2);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("red", 4);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
---
+++ week06/Shapes/Rectangle.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Rectangle : Shape
+{
+   double _length;
+   double _width;
+
+   public Rectangle (string color, double length, double width) : base(color)
+    {
+        _length = length;
+        _width = width;
+    }
+   
+   public override double GetArea()
+    {
+        return _length * _width;
+    }
+}
